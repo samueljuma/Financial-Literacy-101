b77b66c34cc22a51e2f800937577a84f2a073cab
samueljuma <samueljuma1024@gmail.com>
2024-02-07 09:24:10 +0300

    set up nav_graph

--- app/src/main/java/com/sjcreatives/financialliteracy101/data/models/LearningModule.kt
+++ app/src/main/java/com/sjcreatives/financialliteracy101/data/models/LearningModule.kt
@@ -1,7 +1,11 @@
 package com.sjcreatives.financialliteracy101.data.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize // Important especially for passing models as arguments in the nav_graph -> Be sure to add kotlin-parcelize as plugin in gradle
 data class LearningModule (
     val id: Int,
     val title: String,
     val image: Int
-)
+): Parcelable
